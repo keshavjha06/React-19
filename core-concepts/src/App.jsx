47e206578287bdf01b11050c3aade95fb854dfcc
Keshav <keshav@forge.gg>
2026-01-26 15:13:26 +0530

    Styling React components

--- core-concepts/src/App.jsx
+++ core-concepts/src/App.jsx
@@ -13,10 +13,15 @@ import { UserDetails } from './UserDetails'
 import { ProductList } from './ProductList'
 import { NameList } from './NameList'
 import { TodoList } from './TodoList'
+import { Alert } from './Alert'
+import { NewButton } from './NewButton'
 
 function App() {
   return (
     <div>
+      <Alert>Your changes have been saved!</Alert>
+      <Alert type="error">Something went wrong!</Alert>
+      <NewButton />
       <TodoList />
       <NameList />
       <ProductList />
---
+++ core-concepts/src/NewButton.jsx
@@ -0,0 +1,5 @@
+export const NewButton = () => {
+    return (
+        <button className="error">New Button</button>
+    )
+}
