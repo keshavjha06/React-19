a3097ec318ebb1a87d66bff872278cfe16146a1a
Keshav <keshav@forge.gg>
2026-02-01 11:22:07 +0530

    feat: Implement `useState` in `Counter` component and introduce `LogicCard` component demonstrating state management.

--- state/src/App.jsx
+++ state/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import { Counter } from './Counter'
+import { LogicCard } from './LogicCard'
 
 function App() {
   return (
     <>
       <h1>Codevolution</h1>
       <Counter />
+      <Counter />
+      <LogicCard />
     </>
   )
 }
--- state/src/Counter.jsx
+++ state/src/Counter.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react"
+
 export const Counter = () => {
-    let count = 0
-    console.log("counter component rendered");
+    // useState returns an array with two values
+    // currentValue , setterFunction = useState(initialValue)
+    // currentValue , setterFunction = useState(fn)
+
+    const [count, setCount] = useState(() => {
+        console.log("initial state function called");
+        return 0
+    })
+    console.log("counter component rendered with count:", count);
 
     const handleClick = () => {
-        count = count + 1
-        console.log(count);
-
+        setCount(count + 1)
     }
     return <button onClick={handleClick}>Count: {count}</button>
 }
